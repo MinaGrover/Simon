69c71de485cec92f37ba3d4af035da47825bbe4c
Aamina Munzir <aaminamunzir@aaminas-macbook-air.local>
2018-08-24 11:53:23 -0500

    added random generation + images

--- Simon/ViewController.swift
+++ Simon/ViewController.swift
@@ -46,6 +46,9 @@ class ViewController: UIViewController {
     @IBAction func playButtonPressed(_ sender: UIButton)
     {
         //add 3 random #s to sequence
+        //sequence = [Int.random(in: 1...3), Int.random(in: 1...3), Int.random(in: 1...3)] //ONLY IN SWIFT 4.2
+        sequence = [Int(arc4random_uniform(4)+1), Int(arc4random_uniform(4)+1), Int(arc4random_uniform(4)+1)]
+        
         titleLabel.text = "Ready?"
         //delay 1.5 sec (? subject to change)
         titleLabel.text = "Set?"
@@ -73,6 +76,8 @@ class ViewController: UIViewController {
                 else if userSequence.count == sequence.count //check if done with the sequence
                 {
                     //add random # to sequence
+                    sequence += [Int(arc4random_uniform(4)+1)]
+                    
                     playGame()
                 }
             }
@@ -105,19 +110,31 @@ class ViewController: UIViewController {
     {
         if number == 1      //animate top left -- change photo + play sound + change photo back
         {
-            
+            topLeftImageView.image = UIImage(named: "lightGreen")
+            //play sound
+            //pause 0.5
+            topLeftImageView.image = UIImage(named: "darkGreen")
         }
         else if number == 2      //animate top right
         {
-            
+            topLeftImageView.image = UIImage(named: "lightRed")
+            //play sound
+            //pause 0.5
+            topRightImageView.image = UIImage(named: "darkRed")
         }
         else if number == 3      //animate bottom left
         {
-            
+            bottomLeftImageView.image = UIImage(named: "lightYellow")
+            //play sound
+            //pause 0.5
+            topLeftImageView.image = UIImage(named: "darkYellow")
         }
         else if number == 4      //animate bottom right
         {
-            
+            bottomRightImageView.image = UIImage(named: "lightBlue")
+            //play sound
+            //pause 0.5
+            topLeftImageView.image = UIImage(named: "darkBlue")
         }
     }
     
